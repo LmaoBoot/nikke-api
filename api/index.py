670e5d04069815bf1c6cdd68ecf93c9aa9e752ba
LmaoBoot <81543440+LmaoBoot@users.noreply.github.com>
2023-09-08 16:40:20 -0700

    Create index.py

---
+++ api/index.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import request
+import json
+from characters import GetChar
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+
+def home_page():
+    char_name = str(request.args.get('character'))
+    if char_name != "None":
+        return json.dumps(GetChar(char_name))
+    else:
+        return json.dumps({"output":"None"})
